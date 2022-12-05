cca6bbb4528e97c6e0c1d445d1b26152dd5312ed
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2022-12-05 13:05:41 +0700

    Update gitopia.sh

--- Gitopia Testnet Node/gitopia.sh
+++ Gitopia Testnet Node/gitopia.sh
@@ -11,12 +11,12 @@ echo  "   |_______|  |________|  |_________|  \________|  |________|  |__|   |__
 echo -e "\e[0m"
 echo "===========================================================================================" 
 
-echo -e '\e[36mGarapan :\e[39m' Gitopia
-echo -e '\e[36mAuthor :\e[39m' Saujana
-echo -e '\e[36mTelegram :\e[39m' @SaujanaOK
-echo -e '\e[36mTwitter :\e[39m' @SaujanaCrypto
-echo -e '\e[36mDiscord :\e[39m' DEFFAN#0372
-echo -e '\e[36mGithub :\e[39m' https://github.com/SaujanaOK/
+echo -e '\e[36mGarapan      :\e[39m' Gitopia
+echo -e '\e[36mAuthor       :\e[39m' Saujana
+echo -e '\e[36mTelegram     :\e[39m' @SaujanaOK
+echo -e '\e[36mTwitter      :\e[39m' @SaujanaCrypto
+echo -e '\e[36mDiscord      :\e[39m' DEFFAN#0372
+echo -e '\e[36mGithub       :\e[39m' https://github.com/SaujanaOK/
 echo "======================================="
 
 sleep 2
